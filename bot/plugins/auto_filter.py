bfa6091a82851eea487de0ce2f31918d0920ca03
Messi12300 <87484413+Messi12300@users.noreply.github.com>
2021-08-20 17:20:33 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -209,7 +209,7 @@ async def auto_filter(bot, update):
             await bot.send_message(
                 chat_id = update.chat.id,
                 
-                caption=f"<b>🗂️Total File :- {(len_results)} </b>\n<b>🎬Movie Name :-</b> <code>{query}</code>",
+                
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
