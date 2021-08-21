82575e8fc280bb27748cc7c8dfa4ce22158b0227
Messi12300 <87484413+Messi12300@users.noreply.github.com>
2021-08-21 19:10:26 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -208,7 +208,7 @@ async def auto_filter(bot, update):
             await bot.send_message(
                 chat_id = update.chat.id,
                 
-                text=f"⭕Found {(len_results)} Results For Your Request ☺️: <code>{query}</code>",
+                text=f"Total Files: {(len_results)}          Movie Name: <code>{query}</code>             <b>©Mallu cinemas</b>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
